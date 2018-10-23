7da265d059a648b1cf6b7f732f05e5468332bc14
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-10-23 18:17:28 -0500

    Complete

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
 
-    <Provider store={store}>
+    <Provider store ={store}>
+        <BrowserRouter>
         <App />
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();

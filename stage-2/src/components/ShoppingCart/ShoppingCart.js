29e8d6ac22b3032ef416dbc7e5f220f9bd01c259
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-10-23 16:26:25 -0500

    Complete

--- stage-2/src/components/ShoppingCart/ShoppingCart.js
+++ stage-2/src/components/ShoppingCart/ShoppingCart.js
@@ -18,7 +18,8 @@ class ShoppingCart extends Component {
 
     render() {
         let shoppingCartDisplay = this.state.shoppingCart.map((element, index) => {
-            <div className="shopping-cart-product-container">
+            return (
+            <div className="shopping-cart-product-container" key={index}>
                 <img src={element.image} alt="" />
                 <div className="shopping-cart-info">
                     <h2>{element.title}</h2>
@@ -28,6 +29,7 @@ class ShoppingCart extends Component {
                     </div>
                 </div>
             </div>
+            )
         })
         return (
             <div className="shopping-cart-container">

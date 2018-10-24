8e5f28c5fdccb92650dfa9d7c17015cae7401ace
Andrew Montgomery <wamontgomery0068@gmail.com>
2018-10-24 16:20:06 -0500

    Completed

--- stage-3/src/index.js
+++ stage-3/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
 
     <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
